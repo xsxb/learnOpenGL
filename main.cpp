527cf1c025ba4238691b38ba4e2bc96a64738e51
xsxb <ruoyu100@163.com>
2025-06-15 21:02:13 +0200

    Using element buffer object

--- main.cpp
+++ main.cpp
@@ -105,24 +105,40 @@ int main(int, char**){
 
     // VERTICE DATA
     float vertices[] = {
-        -0.5f,  -0.5f,  0.0f,
-        0.5f,   -0.5f,  0.0f,
-        0.0f,   0.5f,   0.0f
+        0.5f,   0.5f,   0.0f,   //top right
+        0.5f,   -0.5f,  0.0f,   //bot right
+        -0.5f,  -0.5f,  0.0f,   //bot left
+        -0.5f,  0.5f,   0.0f    //top left
+    };
+    unsigned int indices[] = {
+        0, 1, 3,
+        1, 2, 3
     };
 
     // Storing vertex data in GPU buffer
-    unsigned int VBO, VAO; //vertex buffer object, vertex array object
+    unsigned int VBO, VAO, EBO; //vertex buffer obj, vertex array obj, element buffer obj
     glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
     glGenVertexArrays(1, &VAO);
 
     glBindVertexArray(VAO);
-
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
 
+    // can unbind after glVertexAttribPointer registered VBO
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+    // Draw wireframe
+    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+    // Can be set to default using:
+    //glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+    
 
     // Render loop
     while(!glfwWindowShouldClose(window))
@@ -134,7 +150,8 @@ int main(int, char**){
 
         glUseProgram(shaderProgram);
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        glBindVertexArray(0);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
